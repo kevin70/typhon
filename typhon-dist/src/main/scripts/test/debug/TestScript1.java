52b3ed77054cbe526dfbef472c27704c9857f033
kevin <kevinz@skfiy.org>
2014-01-16 20:48:17 +0800

    增加获取服务器时间戳接口

--- typhon-dist/src/main/scripts/test/debug/TestScript1.java
+++ typhon-dist/src/main/scripts/test/debug/TestScript1.java
@@ -26,6 +26,6 @@ public class TestScript1 implements Script {
 
     @Override
     public void invoke(Session session, Object obj) {
-        session.setAttribute("TEST_KEY", -5523136583529738319L);
+        session.setAttribute("TEST_KEY", -7983057739438222999L);
     }
 }
